d4f24c9fb7561ac07e44230e7e3b9ffe2cc7c735
陈贤哲 <cxzllsx@qq.com>
2016-03-15 13:41:34 +0800

    Add

---
+++ M2Crm/M2Crm/Pages/Customers/CustomerDetailPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Pasasoft.Crm.ViewModels;
+
+namespace Pasasoft.Crm
+{
+    public partial class CustomerDetailPage : ContentPage
+    {
+        public CustomerViewModel CustomerViewModel {
+            get{ return BindingContext as CustomerViewModel; }
+        }
+
+        public CustomerDetailPage ()
+        {
+            InitializeComponent ();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            Title = CustomerViewModel.Customer.Company.Name;
+        }
+    }
+}
+
---
+++ M2Crm/M2Crm/Pages/Customers/CustomerListPage.cs
@@ -0,0 +1,47 @@
+﻿using Xamarin.Forms;
+
+using Pasasoft.Crm.Views;
+using Pasasoft.Crm.Models;
+using Pasasoft.Crm.ViewModels;
+
+
+namespace Pasasoft.Crm
+{
+    public class CustomerListPage:ContentPage
+    {
+        public CustomerListPage ()
+        {
+            Title = "Customer";
+            Icon = new FileImageSource { File = "customers.png" };
+
+            var view = new CustomerListView();
+            view.ItemSelected += ItemSelected;
+//            view.RowHeight = 80;
+            view.ItemsSource = GetCustomers();
+            Content = view;
+        }
+
+        private async void ItemSelected(object sender,SelectedItemChangedEventArgs e)
+        {
+            var customer = e.SelectedItem as Customer;
+            var customDetailPage = new CustomerDetailPage()
+            {
+                BindingContext = new CustomerViewModel(customer),
+            };
+            await Navigation.PushAsync(customDetailPage);
+        }
+
+
+        private Customer[] GetCustomers()
+        {
+            var customers = new[] {
+                new Customer (){ Contact = new Contact(){ FirstName = "Contact"}, Company = new Company{Name = "Xamarin"}, Address = new Address(){ Country = "USA"} },
+                new Customer (){ Contact = new Contact(){ FirstName = "Contact1"}, Company = new Company{Name = "Microsoft"}, Address = new Address(){ Country = "USA"}  },
+                new Customer (){ Contact = new Contact(){ FirstName = "Contact2"}, Company = new Company{Name = "Xamarin"}, Address = new Address(){ Country = "USA"}  },
+                new Customer (){ Contact = new Contact(){ FirstName = "Contact3"}, Company = new Company{Name = "Microsoft"}, Address = new Address(){ Country = "USA"}  },
+            };
+            return customers;
+        }
+    }
+}
+
